9b278099665c0081f19aae50438c5d895b658dbf
anon <anonXXYZ1@gmail.com>
2024-04-16 18:33:56 +0200

    Created scan.sh for automating

--- comment_scraper/main.py
+++ comment_scraper/main.py
@@ -7,10 +7,10 @@ def main():
     source=requests.get(URL).text
     soup=BeautifulSoup(source,'html.parser')
     comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-    print("## COMMENTS ##\n"+"==========="*3)
+    print(f"## COMMENTS for [{URL}] ##\n"+"==========="*3)
     for c in comments:
         print("#",c)
         c.extract()
-    print("==========="*3)
+    print("==========="*3,"\n")
 if __name__=="__main__":
     main()
