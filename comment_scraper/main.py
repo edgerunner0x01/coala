af4dc97542fe02414903415f57c53774c8b60c58
anon <anonXXYZ1@gmail.com>
2024-04-16 18:15:00 +0200

    Created HTML comment scraper

---
+++ comment_scraper/main.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from bs4 import Comment
+import requests
+import sys
+URL : str = sys.argv[1]
+def main():
+    source=requests.get(URL).text
+    soup=BeautifulSoup(source,'html.parser')
+    comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+    print("## COMMENTS ##\n"+"==========="*3)
+    for c in comments:
+        print("#",c)
+        c.extract()
+    print("==========="*3)
+if __name__=="__main__":
+    main()
