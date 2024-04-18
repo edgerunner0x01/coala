d75206bdfd6b2c8922451c7290a3b4bad002ecf4
anon <anonXXYZ1@gmail.com>
2024-04-18 20:55:09 +0200

    Updated lib/webRecon.py

--- lib/webRecon.py
+++ lib/webRecon.py
@@ -28,16 +28,16 @@ class Target:
             comments=[]
             for comment in extracted_comments:
                 comments.append(str(comment.extract()))
-            return True,comments
-        except Exception as E:
+            return True,comments # RETURNS TRUE OR FLASE BASED OF THE HTTP STATUS CODE 
+        except Exception as E:   # ALONG WITH THE COMMENTS IF THERE IS ANY OR THE ERROR MSG 
             return False,E
     
     def Extract_Emails(self) -> Union[ Tuple[List,bool] , Tuple[str,bool] ] :
         try:
             email_pattern = re.compile(r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})')
             email_matches = re.findall(email_pattern, self.source)
-            return True,email_matches
-        except Exception as E:
+            return True,email_matches  # RETURNS TRUE OR FLASE BASED OF THE HTTP STATUS CODE 
+        except Exception as E:         # ALONG WITH THE EMAILS IF THERE IS ANY OR THE ERROR MSG 
             return False,E
 
     def Extract_Robots(self)  -> Union[ Tuple[str,int,bool] , Tuple[str,bool ] ]: 
@@ -50,9 +50,9 @@ class Target:
             req=requests.get(f"{filter(self.url)}/robots.txt",headers=self.headers)
             status_code=req.status_code
             if int(status_code) == 200:
-                return True,int(status_code),str(req.text)
-            else:
-                return False,int(status_code),""
+                return True,int(status_code),str(req.text) # RETURNS TRUE OR FLASE BASED OF THE HTTP STATUS CODE 
+            else:                                           # ALONG WITH THE HTTP STATUS CODE 
+                return False,int(status_code),""            # AND THE ROBOTS.TXT CONTENT IF THERE IS ANY
         except Exception as E:
             return False,str(E)
 
